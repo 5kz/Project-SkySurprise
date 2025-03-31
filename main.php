90b0d46d0d40e111e2ea0bb50c1cd3dcc6d358ad
5kz <linusakesson@icloud.com>
2025-03-31 14:30:05 +0200

    first commu

---
+++ main.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+    
+</head>
+<body>
+    <div class="header">
+        <div class="headerleft">
+            <a href="support-kontakt.php">Contact and support</a>
+            <a href="om-oss.php">About us</a>
+        </div>
+        <div class="headermiddle">
+            <div class="logo">
+                <a href="main.php"><img src="bilder/skysurpriselogo.png" alt=""></a>
+            </div>
+        </div>
+        <div class="headerright">
+            <a href="logga-in.php">Log in</a>
+        </div>
+    </div>
+    <div class="middlepicture">
+        <div class="bilden">
+            <img src="bilder/bildsidan2.png" alt="">
+            <div class="bildtext">
+                <p>Welcome to SkySurprise</p>
+                
+            </div>
+            <div class="bokaknapp">
+            <a href="boka-resa.php">Book your flight to "PLACEHOLDER" now</a>
+        </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footerinfo">
+            <div class="kortinfo">
+                <h2>SkySurprise</h2>
+                <p>PLACEHOLDER SLOGAN<br> 
+                   PLACEHOLDER.</p>
+            </div>
+            <div class="foretagsinfo">
+                <h3>Contact Us</h3>
+                <p>Email: <a href="mailto:contact@skysurprise.com">contact@skysurprise.com</a></p>
+                <p>Phone: <a href="tel:+46723456789">PLACEHOLDER</a></p>
+                <p>Address: PLACEHOLDER</p>
+            </div>
+            <div class="loggafooter">
+                <img src="bilder/skysurpriselogo.png" alt="">
+            </div>
+        </div>
+        <div class="botten">
+            <p>&copy; 2025 SkySurprise. All rights reserved.</p>
+        </div>
+    </div>
+
